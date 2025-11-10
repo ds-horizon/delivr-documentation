0d4a8dae0dd7a7eaca7a342c72b59346fa2322a4
Prince Tripathi <prince.tripathi@dream11.com>
2025-11-10 15:24:17 +0530

    fix: correct doc base url, reverting unintention change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://delivr.dreamsportslabs.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/delivr-documentation',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
